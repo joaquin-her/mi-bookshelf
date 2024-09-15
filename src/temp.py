5cb281e115b013a87a362be5203a9d1c5a6489c8
Joaquin <hernandez.joaquin346@gmail.com>
2024-09-15 16:47:40 -0300

    solucion con cruzado iterativo y float(-inf)

--- src/temp.py
+++ src/temp.py
@@ -1,56 +1,56 @@
-def buscar_mejor_subarray(arr, inicio=0):
-    if len(arr) == 1:
-        # Caso base: si solo hay un elemento, el mejor subarray es ese elemento
-        return arr, [inicio, inicio + 1], arr[0]
+ENUNCIADO = 13
+"""Dado un arreglo de n enteros (no olvidar que pueden haber números negativos), encontrar el 
+subarreglo contiguo de máxima suma, utilizando División y Conquista. Indicar y 
+	justificar la complejidad del algoritmo.
+	Ejemplo: [5, 3, 2, 4, -1] ->  [5, 3, 2, 4]
+	[5, 3, -5, 4, -1] ->  [5, 3]
+	[5, -4, 2, 4, -1] -> [5, -4, 2, 4]
+	[5, -4, 2, 4] -> [5, -4, 2, 4]
+	[-3, 4, -1, 2, 1, -5] -> [4, -1, 2, 1]
+"""
 
-    medio = len(arr) // 2
+def max_subarray(array):
+	medio = len(array) // 2
+	subarray, suma = buscar_mejor_subarray(array,0, len(array)-1)
+	return subarray
 
-    # Llamadas recursivas para los subarrays izquierdo y derecho
-    arr_i, indices_i, suma_izq = buscar_mejor_subarray(arr[:medio], inicio)
-    arr_d, indices_d, suma_der = buscar_mejor_subarray(arr[medio:], inicio + medio)
+def buscar_mejor_subarray(arr, inicio, fin):
+	if inicio == fin:
+		return arr[inicio:inicio+1], arr[inicio]
 
-    # Encontrar la mejor suma cruzando la mitad
-    suma_centro, a, b = suma_maxima_cruzada(arr, medio, inicio)
+	medio = (inicio + fin)//2
+	arr_i, suma_izq = buscar_mejor_subarray(arr, inicio, medio)
+	arr_d, suma_der = buscar_mejor_subarray(arr, medio +1, fin)
 
-    # Comparar los tres casos: suma izquierda, derecha y cruzada
-    if suma_centro > suma_der and suma_centro > suma_izq:
-        suma_maxima = suma_centro
-        indices = [a, b]
-        mejor_subarray = arr[a - inicio:b - inicio]
-    elif suma_der > suma_izq:
-        suma_maxima = suma_der
-        indices = indices_d
-        mejor_subarray = arr[indices_d[0] - inicio:indices_d[1] - inicio]
-    else:
-        suma_maxima = suma_izq
-        indices = indices_i
-        mejor_subarray = arr[indices_i[0] - inicio:indices_i[1] - inicio]
+	arr_c, suma_centro = buscar_cruzado(arr, inicio, medio, fin)
 
-    return mejor_subarray, indices, suma_maxima
+	if suma_centro > suma_der and suma_centro > suma_izq:
+		return arr_c, suma_centro
+	elif suma_der > suma_izq :
+		return arr_d, suma_der
+	else:
+		return arr_i, suma_izq
 
 
-def suma_maxima_cruzada(arr, medio, inicio):
-    # Calcular la mejor suma hacia la izquierda desde la mitad
-    suma_izquierda = float('-inf')
-    suma = 0
-    mejor_izquierda = medio - 1
-    for i in range(medio - 1, -1, -1):
-        suma += arr[i]
-        if suma > suma_izquierda:
-            suma_izquierda = suma
-            mejor_izquierda = i
+def buscar_cruzado(arr, inicio, medio, fin):
+	suma_maxima_der = float('-inf')
+	suma_maxima_izq = float('-inf')
+	suma = 0
 
-    # Calcular la mejor suma hacia la derecha desde la mitad
-    suma_derecha = float('-inf')
-    suma = 0
-    mejor_derecha = medio
-    for i in range(medio, len(arr)):
-        suma += arr[i]
-        if suma > suma_derecha:
-            suma_derecha = suma
-            mejor_derecha = i
+	mejor_izquierda = medio
+	for i in range(medio, inicio-1, -1):
+		suma += arr[i]
+		if suma > suma_maxima_izq:
+			suma_maxima_izq = suma
+			mejor_izquierda = i
 
-    # Devolver la suma máxima cruzada y los índices
-    return suma_izquierda + suma_derecha, mejor_izquierda + inicio, mejor_derecha + 1 + inicio
-    
-print([-2, 1, -3, 4, -1, 2, 1, -5, 4], max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+	suma = 0
+	mejor_derecha = medio +1
+	for i in range(medio+1, fin+1):
+		suma += arr[i]
+		if suma > suma_maxima_der:
+			suma_maxima_der = suma
+			mejor_derecha = i
+
+	return arr[mejor_izquierda:mejor_derecha+1], suma_maxima_der + suma_maxima_izq
+
