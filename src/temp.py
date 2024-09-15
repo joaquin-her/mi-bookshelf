29280dc817310e2745912adc2374d97dc9206700
Joaquin H <hernandez.joaquin346@gmail.com>
2024-09-15 14:04:52 -0300

    fixed with gpt

--- src/temp.py
+++ src/temp.py
@@ -1,43 +1,58 @@
 
 def max_subarray(array):
-	array, indices, suma_maxima = buscar_mejor_subarray(array)
+	array, indices, suma_maxima = buscar_mejor_subarray(array,0)
 	return array[indices[0]:indices[1]]
 
-def buscar_mejor_subarray(arr):
+def buscar_mejor_subarray(arr, inicio):
 	if len(arr) == 1:
 		indices = [0,1]
 		return arr, indices, arr[0]
 	medio = len(arr)//2
-	arr_i, indices_i, suma_izq = buscar_mejor_subarray(arr[:medio])
-	arr_d, indices_d, suma_der = buscar_mejor_subarray(arr[medio:])
-	suma_centro =suma_izq + suma_der + mi_sum(arr_i[indices_i[1]:] + arr_d[:indices_d[0]])
-	
-	print(arr_i, suma_izq, arr_i+arr_d, suma_der, arr_d)
+    arr_i, indices_i, suma_izq = buscar_mejor_subarray(arr[:medio], inicio)
+    arr_d, indices_d, suma_der = buscar_mejor_subarray(arr[medio:], inicio + medio)
 
-	if suma_centro > suma_der and suma_centro > suma_izq :
-		a = indices_i[0]
-		b = medio + indices_d[1]
-		suma_maxima = suma_centro
-	elif suma_der > suma_izq:
-		a = medio + indices_d[0]
-		b = medio + indices_d[1]
-		suma_maxima = suma_der
-	else:
-		a = indices_i[0]
-		b = indices_i[1]
-		suma_maxima = suma_izq	
-	
-	print(indices_i, indices_d)
+    suma_centro, a, b = suma_maxima_cruzada(arr, medio, inicio)
 
-	print(a,b, '\n')
-	indices = [a, b]
-	return arr, indices, suma_maxima
 
-def mi_sum(array):
-	c = 0
-	for items in array:
-		c += items
-	return c
+    # Comparar los tres casos: suma izquierda, derecha y cruzada
+    if suma_centro > suma_der and suma_centro > suma_izq:
+        suma_maxima = suma_centro
+        indices = [a, b]
+        mejor_subarray = arr[a - inicio:b - inicio]
+    elif suma_der > suma_izq:
+        suma_maxima = suma_der
+        indices = indices_d
+        mejor_subarray = arr[indices_d[0] - inicio:indices_d[1] - inicio]
+    else:
+        suma_maxima = suma_izq
+        indices = indices_i
+        mejor_subarray = arr[indices_i[0] - inicio:indices_i[1] - inicio]
 
+    return mejor_subarray, indices, suma_maxima
+
+    
+def suma_maxima_cruzada(arr, medio, inicio):
+    # Calcular la mejor suma hacia la izquierda desde la mitad
+    suma_izquierda = float('-inf')
+    suma = 0
+    mejor_izquierda = medio - 1
+    for i in range(medio - 1, -1, -1):
+        suma += arr[i]
+        if suma > suma_izquierda:
+            suma_izquierda = suma
+            mejor_izquierda = i
+
+    # Calcular la mejor suma hacia la derecha desde la mitad
+    suma_derecha = float('-inf')
+    suma = 0
+    mejor_derecha = medio
+    for i in range(medio, len(arr)):
+        suma += arr[i]
+        if suma > suma_derecha:
+            suma_derecha = suma
+            mejor_derecha = i
+
+    # Devolver la suma máxima cruzada y los índices
+    return suma_izquierda + suma_derecha, mejor_izquierda + inicio, mejor_derecha + 1 + inicio
 
 print([-2, 1, -3, 4, -1, 2, 1, -5, 4], max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
